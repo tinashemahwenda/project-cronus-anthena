7b6656a5e2ebada6b6503f34981e992685edceb9
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-04-17 02:47:29 +0200

    updated models.py

--- dev/myshop/shop/models.py
+++ dev/myshop/shop/models.py
@@ -9,17 +9,17 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     category = models.ForeignKey(Category,related_name = 'products',on_delete=models.CASCADE)
-    name = models.CharField(max_length=200,db_index=True)
-    slug = models.SlugField(max_length=200,db_update = True)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index= True)
     image = models.ImageField(upload_to = 'products/%Y/%m/%d', blank = True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits = 10, decimal_places=2)
-    availab;e = models.BooleanField(default=True)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now=True)
 
