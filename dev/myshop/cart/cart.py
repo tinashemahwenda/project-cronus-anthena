326e9a6b2fc8a6aa1b5c9a80bcb6f91c00d565f9
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-04-26 15:55:06 +0200

    created cart.py

---
+++ dev/myshop/cart/cart.py
@@ -0,0 +1,41 @@
+from decimal import Decimal
+from django.config import settings
+from shop.models import Product
+
+
+class Cart(object):
+    def __init__(self, request):
+        self.session = request.sessioncart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+
+        self.cart = cart
+    def add(self, product, quantity = 1, override_quantity = False):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = { 'quantity':0, 'price' : str(product.price)}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modeified = True
+
+    def remove(self, product):
+        product_id = str(product_id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in = product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['item_price'] = item['price'] * item['quantity']
+            yield item
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values)
