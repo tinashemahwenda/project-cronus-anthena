de390f0787893ee351d6f70a6b541a923b69b08a
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-04-26 15:58:57 +0200

    created a function to clear a cart session

--- dev/myshop/cart/cart.py
+++ dev/myshop/cart/cart.py
@@ -39,5 +39,10 @@ class Cart(object):
             yield item
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
+
     def get_total_price(self):
         return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values() )
+    
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
