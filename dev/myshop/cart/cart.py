f79ed6516494811fc90362212b23e421ab78e914
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-04-26 15:57:46 +0200

    created a function to get total price in cart

--- dev/myshop/cart/cart.py
+++ dev/myshop/cart/cart.py
@@ -38,4 +38,6 @@ class Cart(object):
             item['item_price'] = item['price'] * item['quantity']
             yield item
     def __len__(self):
-        return sum(item['quantity'] for item in self.cart.values)
+        return sum(item['quantity'] for item in self.cart.values())
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values() )
