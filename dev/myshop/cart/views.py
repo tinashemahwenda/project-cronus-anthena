290ca02d0ec25cfcdd2464566efce83cfb6cdb06
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-04-26 16:14:41 +0200

    created a view to remove product from cart

--- dev/myshop/cart/views.py
+++ dev/myshop/cart/views.py
@@ -16,3 +16,9 @@ def cart_add(request, product_id):
         override_quantity=cd['override']
         )
     return redirect('cart:cart_detail')
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
