1f235b2f62bd291f611bf6856b6187a3ff4a6184
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2021-05-02 03:16:14 +0200

    some few changes

--- dev/myshop/shop/views.py
+++ dev/myshop/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
 from cart.forms import CartAddProductForm
+from .models import Category, Product
 
 def product_list(request, category_slug = None):
     category = None
@@ -9,7 +9,7 @@ def product_list(request, category_slug = None):
 
     if category_slug:
         category = get_object_or_404(Category,slug=category_slug)
-        product = products.filter(category=category)
+        products = products.filter(category=category)
     return render(request, 'shop/product/list.html',{'category':category,
             'categories':categories,
             'products' : products})
